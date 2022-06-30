de8652824b28924956d8d6567c1770f89a6cb121
ElRoulioKFC <julesgeyer@outlook.fr>
2022-06-30 21:51:00 +0200

    1st commit

---
+++ server.js
@@ -0,0 +1,162 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var compteur = 0               ;
+
+
+const fs = require('fs');
+
+let rawdata = fs.readFileSync('produit.json');
+var jso = JSON.parse(rawdata);
+
+var commande = creationCommande(jso);
+
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/session2.html');
+});
+
+http.listen(8080,function(){
+	console.log('serveur en écoute sur le port 8080');
+});
+
+io.on('connect', function(socket){
+	console.log('un client est connecté');
+	var data = getDataFromCompteur(compteur,jso);
+		io.emit('compteur', compteur);
+		io.emit('name', data[0]);
+		io.emit('prix', data[1]);
+		io.emit('quant', data[2]);
+
+	socket.on('minus', function () {
+		addProduct(compteur,commande,jso);
+		io.emit('panier',getPanier(commande,jso));
+		io.emit('total',totalCost(commande,jso));
+	});
+
+	socket.on('plus', function () {
+		addProduct(compteur,commande,jso);
+		io.emit('panier',getPanier(commande,jso));
+		io.emit('total',totalCost(commande,jso));
+	});
+
+	socket.on('cancel', function () {
+		removeAll(commande,json);
+		io.emit('panier', getPanier(commande,jso));
+	});
+
+	socket.on('left', function () {
+		compteur = changeCompteur(-1,compteur,jso);
+		var data = getDataFromCompteur(compteur,jso);
+		io.emit('name', (data[0]));
+		io.emit('prix', (data[1]));
+		io.emit('quant', (data[2]));
+	});
+	socket.on('right', function () {
+		compteur = changeCompteur(1,compteur,jso);
+		var data = getDataFromCompteur(compteur,jso);
+		io.emit('name', (data[0]));
+		io.emit('prix', (data[1]));
+		io.emit('quant', (data[2]));
+		});
+});
+
+function getListName(json) {
+  let list = [];
+	for (var i = 0; i < json.produit.length; i++) {
+		list[i] = json.produit[i].name;
+	}
+	return list;
+}
+function getNameFromCompteur(nb,json){
+	return json.produit[nb].name;
+}
+function getQuantFromCompteur(nb,json){
+	return json.produit[nb].quant;
+}
+function getPrixFromCompteur(nb,json){
+	return json.produit[nb].prix;
+}
+function changeCompteur(i,nb,json){
+	nb += i;
+	if (nb< 0){
+		nb = json.produit.length-1;
+	}
+	if (nb>=json.produit.length){
+			nb = 0;
+	}
+	return nb
+}
+
+function getDataFromCompteur(nb,json){
+	var list = [];
+	list[0] = getNameFromCompteur(nb,json);
+	list[1] = getPrixFromCompteur(nb,json);
+	list[2] = getQuantFromCompteur(nb,json);
+	return list;
+}
+function creationCommande(json){
+	let list = {};
+	for (var i = 0; i < json.produit.length; i++) {
+		list[json.produit[i].name] = 0;
+	}
+	return list;
+}
+
+function addProduct(nb,com,json){
+	com[getNameFromCompteur(nb,json)] += 1;
+	return com;
+}
+function removeProduct(nb,com,json){
+	if (com[getNameFromCompteur(nb,json)] > 1){
+		com[getNameFromCompteur(nb,json)] -= 1;
+	}
+	return com;
+}
+function removeAll(com,json){
+	return creationCommande(json);
+}
+function getCompteurFromName(nom,json){
+	let list = {};
+	for (var i = 0; i < json.produit.length; i++) {
+		if (json.produit[i].name == nom) return i;
+	}
+	return -1;
+}
+function getPanier(com,json){
+	var list = getListName(json);
+	var res = "";
+	for (var i in list) {
+		var name = list[i];
+   if (com[name] > 0 ) {
+	 res += (name + " : quantité " + com[name] + "  quantité totale : " +(com[name] * json.produit[getCompteurFromName(name,json)].quant) + "g"+ "  prix total : " +(com[name] * json.produit[getCompteurFromName(name,json)].prix) +"\n");
+}}
+	return res;
+}
+function totalCost(com,json){
+	var list = getListName(json);
+	var prix = 0;
+	var poidsTotal = 0;
+	for (var i in list) {
+		var name = list[i];
+   if (com[name] > 0 ) {
+		 prix += (com[name] * json.produit[getCompteurFromName(name,json)].prix);
+		 poidsTotal += (com[name] * json.produit[getCompteurFromName(name,json)].quant);
+		 console.log(poidsTotal);
+}}
+	if (poidsTotal < 20000){
+		var livraison = 20;
+	}else if (poidsTotal < 40000){
+		var livraison = 40;
+	}else if (poidsTotal< 60000){
+		var livraison = 50;
+	}else {
+		var livraison = 70
+	}
+		if (prix > 0){
+		return "cout total : " + (prix + livraison) + " dont livraison(" + livraison + ")";
+	}else {
+		return "";
+	}
+}
